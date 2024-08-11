1158b97bb4f134a4b13bf59cee296fe7f075c308
Henrique <henriq2303@gmail.com>
2024-08-11 20:32:00 -0300

    test2

--- src/DefaultLayout/index.tsx
+++ src/DefaultLayout/index.tsx
@@ -1,14 +1,12 @@
-// import { Outlet } from 'react-router-dom'
 import { Header } from '../components/Header'
 import { LayoutContainer } from './styles'
-import { Home } from '../pages/Home'
+import { Outlet } from 'react-router-dom'
 
 export function DefaultLayout() {
   return (
     <LayoutContainer>
       <Header />
-      <Home />
-      {/* <Outlet /> */}
+      <Outlet />
     </LayoutContainer>
   )
 }
